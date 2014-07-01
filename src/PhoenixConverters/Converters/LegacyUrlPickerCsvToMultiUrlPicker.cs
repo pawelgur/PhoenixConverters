a0685e105888b006fd9b8e7aa2c65fc703d4e4b1
Pavel Gurecki <pavel.gurecki@adform.com>
2014-07-01 11:41:31 +0300

    Initiall commit for Legacy Url Picker in CSV to v7 Multi Url Picker converter. NOTE: was not tested thoroughly.

---
+++ src/PhoenixConverters/Converters/LegacyUrlPickerCsvToMultiUrlPicker.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PhoenixConverters.Abstract;
+using PhoenixConverters.Models;
+using umbraco.cms.helpers;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+
+namespace PhoenixConverters.Converters
+{
+    /// <summary>
+    /// Converts v6 Url Picker in CSV format to v7 Multi Url Picker
+    /// </summary>
+    public class LegacyUrlPickerCsvToMultiUrlPicker : DataTypeConverterBase
+    {
+        readonly IMediaService mediaService = ApplicationContext.Current.Services.MediaService;
+        readonly IContentService contentService = ApplicationContext.Current.Services.ContentService;
+        
+        
+        public override string Alias
+        {
+            get { return "urlPickerCsvToMultiUrlPicker"; }
+        }
+
+        public override string Name
+        {
+            get { return "Url Picker CSV to Multi Url picker v7"; }
+        }
+
+        public override string ConvertTo
+        {
+            get { return "RJP.MultiUrlPicker"; }
+        }
+
+        public override Models.ConversionResult Convert(int sourceDataTypeId, int targetDataTypeId, bool updatePropertyTypes, bool publish, bool preview = true)
+        {
+            var result = new ConversionResult(Services, sourceDataTypeId, targetDataTypeId, updatePropertyTypes, publish, preview);
+
+            result = result.Convert(convert);
+            result.Message = "Complete.";
+
+            if (result.SuccessfulConversions >= result.FailedConversions)
+            {
+                result.IsCompatible = true;
+            }
+            else
+            {
+                result.IsCompatible = false;
+            }
+
+            return result;
+        }
+
+        private string convert(string input)
+        {
+            /*
+             * "Url Picker Mode, New Window, Node ID, URL, Link Title"
+             * 
+             * to (some props are not required)
+             * 
+             * [{      
+             *      "id": 0,
+             *      "name": "",
+             *      "url": "",
+             *      "isMedia": true,
+             *      "icon": "icon-link",
+             *      "target": "_blank"
+             *  }]
+             * 
+             */
+
+            try
+            {
+                var urlPickerState = PG.UmbracoExtensions.Helpers.UrlPicker.UrlPickerState.Deserialize(input);
+                var model = new UrlPickerV7(urlPickerState);
+
+                //proper name
+                if (String.IsNullOrWhiteSpace(model.name) && model.id > 0)
+                {
+                    int id = model.id ?? default(int);
+
+                    if (model.isMedia)
+                    {
+                        var mediaItem = mediaService.GetById(id);
+                        model.name = mediaItem == null ? "Media item" : mediaItem.Name;
+                    }
+                    else
+                    {
+                        var contentItem = contentService.GetById(id);
+                        model.name = contentItem == null ? "Content item" : contentItem.Name;
+                    }
+                    
+                }
+
+                //return blank array if no url
+                var array = String.IsNullOrWhiteSpace(model.url) ? new object[] { } : new[] { model };
+
+                return JsonConvert.SerializeObject(array, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }); 
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/PhoenixConverters/Models/UrlPickerV7.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixConverters.Models
+{
+    public class UrlPickerV7
+    {
+        public int? id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public bool isMedia { get; set; }
+        public string icon { get; set; }
+        public string target { get; set; }
+
+        public UrlPickerV7()
+        {
+            icon = "icon-link";
+        }
+
+        public UrlPickerV7(PG.UmbracoExtensions.Helpers.UrlPicker.UrlPickerState urlPickerState)
+            :   this()
+        {
+            id = urlPickerState.NodeId;
+            name = urlPickerState.Title;
+            url = urlPickerState.Url;
+            isMedia = urlPickerState.Mode == PG.UmbracoExtensions.Helpers.UrlPicker.UrlPickerMode.Media;
+            target = urlPickerState.NewWindow ? "_blank" : null;
+        }
+
+    }
+}
